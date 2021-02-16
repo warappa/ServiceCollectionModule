ab6a21374fbaf3e16474ae88955ead0afeffa74f
warappa <spoiltnr1@gmx.at>
2021-02-16 22:16:24 +0100

    Add IServiceCollectionModule with AddModule extension-method

---
+++ Warappa.Microsoft.Extensions.DependencyInjection.ServiceCollectionModule/IServiceCollectionModule.cs
@@ -0,0 +1,14 @@
+﻿namespace Microsoft.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// Groups related service configurations.
+    /// </summary>
+    public interface IServiceCollectionModule
+    {
+        /// <summary>
+        /// Adds the service configurations of this module to the provided <see cref="IServiceCollection"/> instance.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/> to add the service configurations of this module to.</param>
+        void ConfigureServices(IServiceCollection services);
+    }
+}
---
+++ Warappa.Microsoft.Extensions.DependencyInjection.ServiceCollectionModule/ServiceCollectionModuleExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionModuleExtensions
+    {
+        /// <summary>
+        /// Adds the service configurations of the provided <see cref="IServiceCollectionModule"/> instances to the <see cref="IServiceCollection"/> instance.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/> to add the service configurations of the modules to.</param>
+        /// <param name="modules">The list of <see cref="IServiceCollectionModule"/> instances.</param>
+        /// <returns>A reference to the provided <see cref="IServiceCollection"/> instance after the operation has completed.</returns>
+        public static IServiceCollection AddModules(this IServiceCollection services, params IServiceCollectionModule[] modules)
+        {
+            foreach (var module in modules)
+            {
+                module.ConfigureServices(services);
+            }
+
+            return services;
+        }
+    }
+}
